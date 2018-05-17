e3ba6d67bde14d2f56837a676b3aff9a440b4c9a
liususan091219 <liususan091219@gmail.com>
2018-05-17 02:13:53 -0500

    remove redundant files

--- count_query.py
+++
@@ -1,8 +0,0 @@
-fin = open("../MatchZoo_data/result/python/dssm.tit.txt", "r")
-uniqueset = set([])
-
-for line in fin:
-	tokens = line.strip("\n").split("\t")
-	uniqueset.add(tokens[0])
-
-print(len(uniqueset))
--- test_missed.py
+++
@@ -1,7 +0,0 @@
-fin = open("../MatchZoo_data/result/java/dssm.title.txt", "r")
-qidset = set()
-for line in fin:
-	tokens = line.strip("\n").split("\t")
-	qidset.add(tokens[0])
-
-print(len(qidset))	
