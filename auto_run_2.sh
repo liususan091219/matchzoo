190a3d733051fd5495b1303eb1a68c09d7175753
liususan091219 <liususan091219@gmail.com>
2018-05-24 10:58:47 -0500

    [added] all config]

---
+++ auto_run_2.sh
@@ -0,0 +1,3 @@
+python matchzoo/main.py --phase train --model_file configs/python/drmm_tks.ques.config
+python matchzoo/main.py --phase train --model_file configs/python/knrm.ques.config
+python matchzoo/main.py --phase train --model_file configs/python/conv_knrm.ques.config
