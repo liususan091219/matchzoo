95584165a071c40ec31081b810943756bd1d5b6a
liususan091219 <liususan091219@gmail.com>
2018-05-29 22:41:54 -0500

    [rm] redundant files

--- auto_run_2.sh
+++
@@ -1,3 +0,0 @@
-python matchzoo/main.py --phase train --model_file configs/python/drmm_tks.ques.config
-python matchzoo/main.py --phase train --model_file configs/python/knrm.ques.config
-python matchzoo/main.py --phase train --model_file configs/python/conv_knrm.ques.config
--- auto_run_3.sh
+++
@@ -1,2 +0,0 @@
-python matchzoo/main.py --phase train --model_file configs/python/anmm.tit.config
-
