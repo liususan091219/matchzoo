b885c9c6369d4c1a4fe45408868859c96c4b8baf
liususan091219 <xliu93@illinois.edu>
2018-07-31 01:24:14 -0500

    __init__.py

--- matchzoo/inputs/__init__.py
+++ matchzoo/inputs/__init__.py
@@ -10,10 +10,12 @@ from .point_generator import DRMM_PointGenerator
 from .pair_generator import PairGenerator
 from .pair_generator import Triletter_PairGenerator
 from .pair_generator import DRMM_PairGenerator
+from .pair_generator import DRMM_PairGenerator_linear
 from .pair_generator import PairGenerator_Feats
 from .list_generator import ListGenerator
 from .list_generator import Triletter_ListGenerator
 from .list_generator import DRMM_ListGenerator
+from .list_generator import DRMM_ListGenerator_linear
 from .list_generator import ListGenerator_Feats
 
 def serialize(generator):
