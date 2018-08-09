5691e4fea235ab4463eaf3539357c8f8b0ea5c66
liususan091219 <xliu93@illinois.edu>
2018-08-09 12:01:17 -0500

    dot

--- matchzoo/models/anmm_linear.py
+++ matchzoo/models/anmm_linear.py
@@ -90,8 +90,8 @@ class ANMM_linear(BasicModel):
         out_title = Dot(axes= [1, 1])([title_z, q_w])
         out_question = Dot(axes = [1, 1])([question_z, q_w])
         out_answer = Dot(axes = [1, 1])([answer_z, q_w])
-        K_param = K.constant(value=[0.5, 0.3, 0.2], dtype="float32")
+        K_param = K.constant(value=[[0.5, 0.3, 0.2]], dtype="float32")
         out_ = Concatenate()([out_title, out_question, out_answer])
-        out_ = K.dot(out_, K.transpose(K_param))
+        out_ = Lambda(lambda x: K.dot(x, K.transpose(K_param)), output_shape(1,))(out_)
         model = Model(inputs=[query, title, question, answer], outputs=[out_])
         return model
