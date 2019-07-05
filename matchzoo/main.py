2d6bdcd20d56f732edb30334d58d1be979ab9243
liususan091219 <xliu93@illinois.edu>
2019-07-04 20:59:43 -0500

    commit

--- matchzoo/main.py
+++ matchzoo/main.py
@@ -51,29 +51,29 @@ def eval_score(y_true, y_pred, eval_name, eval_func):
 		return eval_func(y_true = y_true, y_pred = y_pred)
 
 def mrr(labellist):
-        score =0
-        scorecount = 0
-        for i in range(0, len(labellist)):
-                if labellist[i] == "1":
-                        score += 1.0 / (i + 1.0)
-                        scorecount += 1
-        if scorecount == 0:
-                return 0
-        else:
-                return score / scorecount
+    score =0
+    scorecount = 0
+    for i in range(0, len(labellist)):
+            if labellist[i] == "1":
+                    score += 1.0 / (i + 1.0)
+                    scorecount += 1
+    if scorecount == 0:
+            return 0
+    else:
+            return score / scorecount
 
 def ndcg(labellist, topK):
-        dcg = idcg = 0.0
-        for i in range(0, min(topK, len(labellist))):
-                if labellist[i] == "1":
-                        dcg += 1.0 / math.log(float(i + 2), 2.0)
-        relcount = 0
-        for i in range(0, len(labellist)):
-                if labellist[i] == "1":
-                        relcount += 1
-        for i in range(0, min(topK, relcount)):
-                idcg += 1.0 / math.log(float(i + 2), 2.0)
-	return dcg / idcg
+    dcg = idcg = 0.0
+    for i in range(0, min(topK, len(labellist))):
+            if labellist[i] == "1":
+                    dcg += 1.0 / math.log(float(i + 2), 2.0)
+    relcount = 0
+    for i in range(0, len(labellist)):
+            if labellist[i] == "1":
+                    relcount += 1
+    for i in range(0, min(topK, relcount)):
+            idcg += 1.0 / math.log(float(i + 2), 2.0)
+    return dcg / idcg
 
 
 def load_model(config):
